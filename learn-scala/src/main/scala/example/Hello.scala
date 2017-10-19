5c1bf621564cd36ec0ab3ce878c1f414181a9475
Nui Narongwet <narongwet.m@gmail.com>
2017-10-19 20:54:28 +0700

    Added more code

--- learn-scala/src/main/scala/example/Hello.scala
+++ learn-scala/src/main/scala/example/Hello.scala
@@ -5,8 +5,28 @@ import fibonacci.Fibonacci
 object Hello extends Greeting with App {
   println(greeting)
   println(Fibonacci.fib(10))
+  tryFor()
+  def twice(f:Int=>Int) = f compose f
+  println(twice(_+3)(7))
 }
+
+case class Weekday(name: String, index: Int)
 
 trait Greeting {
   lazy val greeting: String = "hello"
+
+  def tryFor(): Unit = {
+    val week = List(
+      Weekday("Monday", 1),
+      Weekday("Tuesday", 2),
+      Weekday("Wednesday", 3),
+      Weekday("Thursday", 4),
+      Weekday("Friday", 5),
+      Weekday("Saturday", 6),
+      Weekday("Sunday", 7)
+    )
+    val workdays = for (d <- week if d.index < 6)
+      yield d
+    workdays.foreach(d => println(d.name))
+  }
 }
